322ee60d008d3ee8cf9d482561933a01ff7be20f
rahulc <rahulc@opentext.com>
2024-07-08 10:49:36 +0530

    updated home.js file to use default user if no user logged in

--- home.js
+++ home.js
@@ -7,8 +7,8 @@ const emailContainer = document.getElementById('emailContainer');
 
 const {full_name, email} = storage;
 
-nameContainer.innerText = full_name;
-emailContainer.innerText = email;
+nameContainer.innerText = full_name === undefined ? "user" : full_name;
+emailContainer.innerText = email === undefined ? "user@gmail.com" : email;
 
 const logoutUser = () => {
     storage.clear
